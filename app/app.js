1daf238d3030febaa97276259fa1cb0036012baf
turt2live <travpc@gmail.com>
2017-03-19 22:48:21 -0600

    Add some force to have the graph move apart a bit better

--- app/app.js
+++ app/app.js
@@ -16,7 +16,7 @@ var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id(function (d) {
         return d.id;
     }))
-    .force("charge", d3.forceManyBody())
+    .force("charge", d3.forceManyBody().strength(-150))
     .force("center", d3.forceCenter(width / 2, height / 2));
 
 var source = "api/v1/network"; // test.json
