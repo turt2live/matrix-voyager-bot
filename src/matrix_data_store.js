db1bac309f574dc4b84f61a636b3672b7588d8f0
turt2live <travpc@gmail.com>
2017-03-25 00:10:36 -0600

    Actually store sync token in localstorage
    Hopefully addresses #38

--- src/matrix_data_store.js
+++ src/matrix_data_store.js
@@ -2,5 +2,15 @@ var LocalStorage = require("node-localstorage").LocalStorage;
 var MatrixInMemoryStore = require("matrix-js-sdk").MatrixInMemoryStore;
 
 var localStorage = new LocalStorage("db/localstorage");
-var store = new MatrixInMemoryStore({localStorage: localStorage});
+var store = new MatrixInMemoryStore(localStorage);
+
+// override sync token functions
+store.setSyncToken = function (token) {
+    localStorage.setItem("voyager-sync-token", token);
+};
+
+store.getSyncToken = function () {
+    return localStorage.getItem("voyager-sync-token");
+};
+
 module.exports = store;
