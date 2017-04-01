bfe30e504218423d23870444cac350c073acaba5
turt2live <travpc@gmail.com>
2017-04-01 11:17:00 -0600

    Add web frontend template
    Based off of https://github.com/preboot/angular-webpack

---
+++ web/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { ApiService } from './shared';
+
+import '../style/app.scss';
+
+@Component({
+  selector: 'my-app', // <my-app></my-app>
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  url = 'https://github.com/preboot/angular2-webpack';
+  title: string;
+
+  constructor(private api: ApiService) {
+    this.title = this.api.title;
+  }
+}
