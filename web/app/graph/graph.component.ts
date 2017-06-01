a3406eef9d26c482193011a279aed521be1f462c
turt2live <travpc@gmail.com>
2017-05-31 22:22:05 -0600

    Broken start to canvas rendering
    Needs:
    * Proper zoom
    * Proper click & drag
    * Not to be shown off-screen
    * Proper node rendering
    * Tooltips/hover effects
    * Clickability for modal
    
    This is a start to #96

--- web/app/graph/graph.component.ts
+++ web/app/graph/graph.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit, ElementRef } from "@angular/core";
 import { ApiService } from "../shared/api.service";
 import { VoyagerNetwork } from "../shared/voyager-network";
 import { D3Service, D3, Selection, ForceLink } from "d3-ng2-service";
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { NetworkLink, NetworkNode } from "./network-dto";
-import { GraphDialogComponent } from "./graph-dialog/graph-dialog.component";
-import { LocalStorageService } from 'angular-2-local-storage';
+import { LocalStorageService } from "angular-2-local-storage";
 
 @Component({
     selector: 'my-graph',
@@ -17,11 +15,11 @@ export class GraphComponent implements OnInit {
     private parentNativeElement: any;
     private data: {links: NetworkLink[], nodes: NetworkNode[], nodeLinks: string[]};
 
-    public highlightedNode: NetworkNode = null;
-    public highlightedLink: NetworkLink = null;
-    private isDragging = false;
+    // public highlightedNode: NetworkNode = null;
+    // public highlightedLink: NetworkLink = null;
+    // private isDragging = false;
 
-    constructor(private api: ApiService, element: ElementRef, d3Service: D3Service, private modalService: NgbModal, private localStorageService: LocalStorageService) {
+    constructor(private api: ApiService, element: ElementRef, d3Service: D3Service, /*private modalService: NgbModal,*/ private localStorageService: LocalStorageService) {
         this.d3 = d3Service.getD3();
         this.parentNativeElement = element.nativeElement;
     }
@@ -80,266 +78,187 @@ export class GraphComponent implements OnInit {
 
         d3ParentElement = d3.select(this.parentNativeElement);
 
-        let nodeTooltip = d3ParentElement.select<HTMLDivElement>("div.tooltip.node-tooltip");
-        let linkTooltip = d3ParentElement.select<HTMLDivElement>("div.tooltip.link-tooltip");
+        // let nodeTooltip = d3ParentElement.select<HTMLDivElement>("div.tooltip.node-tooltip");
+        // let linkTooltip = d3ParentElement.select<HTMLDivElement>("div.tooltip.link-tooltip");
 
-        let svg = d3ParentElement.select<SVGSVGElement>("svg");
+        let canvasElement = d3ParentElement.select<HTMLCanvasElement>("canvas");
         let bbox = d3ParentElement.node().getBoundingClientRect();
         let width = bbox.width;
         let height = bbox.height;
+        let canvas = canvasElement.node().getContext("2d");
 
-        svg.attr("width", width).attr("height", height);
+        canvasElement.attr("width", width).attr("height", height);
 
-        svg.call(d3.zoom()
+        canvasElement.call(d3.zoom()
             .scaleExtent([-1, 10])
             .on('zoom', () => {
-                svg.select("g.links")
-                    .attr("transform",
-                        "translate(" + d3.event.transform.x + "," + d3.event.transform.y + ")" +
-                        "scale(" + d3.event.transform.k + "," + d3.event.transform.k + ")");
-                svg.select("g.nodes")
-                    .attr("transform",
-                        "translate(" + d3.event.transform.x + "," + d3.event.transform.y + ")" +
-                        "scale(" + d3.event.transform.k + "," + d3.event.transform.k + ")");
+                canvas.save();
+                canvas.clearRect(0, 0, width, height);
+                canvas.translate(d3.event.translate[0], d3.event.translate[1]);
+                canvas.scale(d3.event.scale, d3.event.scale);
+                this.render(canvas, this.data.nodes, this.data.links);
+                canvas.restore();
             }));
 
         this.processNetwork(network);
 
-        let defs = svg.select<SVGDefsElement>("defs");
-        this.buildFills(defs);
-
         this.localStorageService.set('seenNodes', this.data.nodes.map(n => n.id));
 
-        let simulation = d3.forceSimulation()
+        let simulation = d3.forceSimulation(this.data.nodes)
             .force("link", d3.forceLink<NetworkNode, NetworkLink>()
                 .id(n => <any>n.id)
                 .distance(k => Math.sqrt(k.value) * 75))
             .force("charge", d3.forceManyBody<NetworkNode>()
                 .strength(n => Math.max(-400, n.linkCount * -40)))
             .force("center", d3.forceCenter(width / 2, height / 2))
-            .force("collide", d3.forceCollide<NetworkNode>(n => n.type == 'room' ? 20 : 15).strength(0.5));
+            .force("collide", d3.forceCollide<NetworkNode>(n => n.type === 'room' ? 20 : 15).strength(0.5));
 
-        let links = svg.append("g")
-            .attr("class", "links")
-            .selectAll("path")
-            .data(this.data.links).enter().append("svg:path")
-            .attr("fill", "none")
-            .attr("stroke-width", k => Math.sqrt(k.value))
-            .attr("stroke", k => this.getColorForType(k.type))
-            .attr("stroke-opacity", 0.7);
-
-        let nodes = svg.append("g")
-            .attr("class", "nodes")
-            .selectAll("circle")
-            .data(this.data.nodes).enter().append("circle")
-            .attr("fill", n => "url(#fillFor" + n.id + ")")
-            .attr("r", n => n.type == 'room' ? 15 : 10)
-            .attr("stroke", "#fff")
-            //.attr("stroke-dasharray", "20,10")
-            .attr("stroke-width", n => n.type == 'room' ? '1.5px' : '1px')
-            .attr("cursor", "pointer")
-            .call(d3.drag<SVGCircleElement, any>()
-                .on("start", d => {
-                    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-                    d.fx = d.x;
-                    d.fy = d.y;
-                    this.isDragging = true;
-                })
-                .on("drag", d => {
-                    d.fx = d3.event.x;
-                    d.fy = d3.event.y;
-                })
-                .on("end", d => {
-                    if (!d3.event.active) simulation.alphaTarget(0);
-                    d.fx = null;
-                    d.fy = null;
-                    this.isDragging = false;
-                }));
-
-        nodes.on('click', n => {
-            if (n) {
-                console.log("Dialog disabled");
-                return;
-            }
-            let modalInstance = this.modalService.open(GraphDialogComponent);
-            modalInstance.componentInstance.node = n;
-            svg.style("z-index", -1);
-            modalInstance.result.then(() => svg.style("z-index", 0), () => svg.style("z-index", 0));
-        });
-
-        nodes.on('mouseover', n => {
-            if (this.isDragging) return;
-
-            this.fade(n, 0.1, nodes, links);
-
-            this.highlightedNode = n;
-            nodeTooltip.transition().duration(200).style("opacity", 0.9);
-            nodeTooltip.style("left", d3.event.pageX + "px");
-            nodeTooltip.style("top", d3.event.pageY + "px");
-        });
-
-        nodes.on('mouseout', n => {
-            if (this.isDragging) return; // Don't release node when dragging
-
-            this.fade(n, 1, nodes, links);
-
-            // this.highlightedNode = null;
-            nodeTooltip.transition().duration(500).style('opacity', 0);
-        });
-
-        links.on('mouseover', k => {
-            this.highlightedLink = k;
-            linkTooltip.transition().duration(200).style("opacity", 0.9);
-            linkTooltip.style("left", d3.event.pageX + "px");
-            linkTooltip.style("top", d3.event.pageY + "px");
-        });
-
-        links.on('mouseout', () => {
-            // this.highlightedLink = null;
-            linkTooltip.transition().duration(500).style("opacity", 0);
-        });
-
-        simulation.nodes(this.data.nodes).on('tick', () => this.onTick(links, nodes));
+        simulation.on('tick', () => this.onTick(canvas, this.data.links, this.data.nodes, width, height));
         simulation.force<ForceLink<NetworkNode, NetworkLink>>("link").links(this.data.links);
+
+        // TODO: Dragging
+        // ref: https://bl.ocks.org/mbostock/1b64ec067fcfc51e7471d944f51f1611
     }
 
-    private onTick(links, nodes) {
-        nodes.attr("cx", d => d.x)
-            .attr("cy", d => d.y);
+    private onTick(canvas, links, nodes, width, height) {
+        canvas.clearRect(0, 0, width, height);
+        canvas.save();
+        canvas.translate(width / 2, height / 2);
+        this.render(canvas, nodes, links);
+        canvas.restore();
+    }
 
-        links.attr("d", d => {
-            let dx = (d.target.x - d.source.x) / 0.1;
-            let dy = (d.target.y - d.source.y) / 0.1;
-            let dr = Math.sqrt((dx * dx) + (dy * dy));
-
-            let hasRelatedLinks = d.relatedLinkTypes && d.relatedLinkTypes.length > 1;
-            if (!hasRelatedLinks && (d.inverseCount == 0 || d.value == 0)) {
-                return "M" + d.source.x + "," + d.source.y + " L" + d.target.x + "," + d.target.y;
-            }
-
-            let shouldInvert = hasRelatedLinks ? (d.relatedLinkTypes.indexOf(d.type) !== 0) : false;
-
-            let sx = shouldInvert ? d.target.x : d.source.x;
-            let sy = shouldInvert ? d.target.y : d.source.y;
-            let tx = shouldInvert ? d.source.x : d.target.x;
-            let ty = shouldInvert ? d.source.y : d.target.y;
-            return "M" + sx + "," + sy + "A" + dr + "," + dr + " 0 0,1 " + tx + "," + ty;
+    private render(canvas, nodes, links) {
+        canvas.beginPath();
+        links.forEach(k => {
+            canvas.moveTo(k.source.x, k.source.y);
+            canvas.lineTo(k.target.x, k.target.y);
         });
+        canvas.strokeStyle = "#ccc";
+        canvas.stroke();
+
+        canvas.beginPath();
+        nodes.forEach(n => {
+            canvas.moveTo(n.x + 5, n.y);
+            canvas.arc(n.x, n.y, 3, 0, 2 * Math.PI);
+        });
+        canvas.strokeStyle = "#f00";
+        canvas.stroke();
     }
 
-    private fade(selfNode, opacity: number, nodes, links) {
-        nodes.attr('stroke-opacity', n => this.isConnected(selfNode, n) ? 1 : opacity);
-        nodes.attr('fill-opacity', n => this.isConnected(selfNode, n) ? 1 : opacity);
+    // private fade(selfNode, opacity: number, nodes, links) {
+    //     nodes.attr('stroke-opacity', n => this.isConnected(selfNode, n) ? 1 : opacity);
+    //     nodes.attr('fill-opacity', n => this.isConnected(selfNode, n) ? 1 : opacity);
+    //
+    //     links.attr("stroke-opacity", k => (k.source === selfNode || k.target === selfNode ? 1 : opacity));
+    // }
 
-        links.attr("stroke-opacity", k => (k.source === selfNode || k.target === selfNode ? 1 : opacity));
-    }
+    // private isConnected(node1: NetworkNode, node2: NetworkNode) {
+    //     if (node1.id === node2.id) return true;
+    //
+    //     const sourceToTarget = node1.id + "," + node2.id;
+    //     const targetToSource = node2.id + "," + node1.id;
+    //
+    //     return this.data.nodeLinks.indexOf(sourceToTarget) !== -1 || this.data.nodeLinks.indexOf(targetToSource) !== -1;
+    // }
+    //
+    // private buildFills(defs) {
+    //     const seenNodes = this.localStorageService.get<number[]>('seenNodes') || [];
+    //
+    //     for (let node of this.data.nodes) {
+    //         const fillKey = "fillFor" + node.id;
+    //         const radius = node.type === 'room' ? 15 : 10;
+    //
+    //         let pattern = defs.append("pattern")
+    //             .attr("id", fillKey)
+    //             .attr("x", "0%").attr("y", "0%")
+    //             .attr("width", "100%").attr("height", "100%")
+    //             .attr("viewBox", "0 0 " + radius + " " + radius);
+    //         pattern.append("rect")
+    //             .attr("width", radius).attr("height", radius)
+    //             .attr("fill", "#fff");
+    //
+    //         if (node.avatarUrl && node.avatarUrl.trim().length > 0) {
+    //             pattern.append("image")
+    //                 .attr("x", "0%").attr("y", "0%")
+    //                 .attr("width", radius).attr("height", radius)
+    //                 .attr("xlink:href", node.avatarUrl);
+    //         } else {
+    //             let text = node.name[0];
+    //             if (text === '!' || text === '@' || text === '#')
+    //                 text = node.name[1];
+    //             if (!text || node.isAnonymous) {
+    //                 text = node.type === 'room' ? "#" : "@";
+    //             }
+    //
+    //             let size = node.type === 'room' ? 8 : 6;
+    //
+    //             pattern.append("rect")
+    //                 .attr("width", radius).attr("height", radius)
+    //                 .attr("fill", this.getBackgroundForString(node.name));
+    //             pattern.append("text")
+    //                 .attr("text-anchor", "middle")
+    //                 .attr("dominant-baseline", "central")
+    //                 .attr("alignment-baseline", "central")
+    //                 .attr("x", radius / 2).attr("y", radius / 2)
+    //                 .attr("font-family", "sans-serif")
+    //                 .attr("font-size", size)
+    //                 .attr("fill", "#fff")
+    //                 .text(text);
+    //         }
+    //
+    //         let isNew = seenNodes.length > 0 && (seenNodes.indexOf(node.id) === -1);
+    //         if (!isNew || node.type === 'user') continue;
+    //
+    //         pattern.append("rect")
+    //             .attr("width", radius * 2).attr("height", 12)
+    //             .attr("fill", "#f9d35e")
+    //             .attr("stroke", "#a0a0a0")
+    //             .attr("stroke-width", "1px")
+    //             .attr("transform", "translate(0) rotate(45 " + radius + " 6)");
+    //         pattern.append("text")
+    //             .attr("text-anchor", "middle")
+    //             .attr("dominant-baseline", "central")
+    //             .attr("alignment-baseline", "central")
+    //             .attr("x", 11).attr("y", 9)
+    //             .attr("transform", "rotate(45 " + radius + " 6)")
+    //             .attr("font-family", "sans-serif")
+    //             .attr("font-size", 4)
+    //             .attr("fill", "#000")
+    //             .text("NEW");
+    //     }
+    // }
 
-    private isConnected(node1: NetworkNode, node2: NetworkNode) {
-        if (node1.id == node2.id) return true;
-
-        const sourceToTarget = node1.id + "," + node2.id;
-        const targetToSource = node2.id + "," + node1.id;
-
-        return this.data.nodeLinks.indexOf(sourceToTarget) !== -1 || this.data.nodeLinks.indexOf(targetToSource) !== -1;
-    }
-
-    private buildFills(defs) {
-        const seenNodes = this.localStorageService.get<number[]>('seenNodes') || [];
-
-        for (let node of this.data.nodes) {
-            const fillKey = "fillFor" + node.id;
-            const radius = node.type == 'room' ? 15 : 10;
-
-            let pattern = defs.append("pattern")
-                .attr("id", fillKey)
-                .attr("x", "0%").attr("y", "0%")
-                .attr("width", "100%").attr("height", "100%")
-                .attr("viewBox", "0 0 " + radius + " " + radius);
-            pattern.append("rect")
-                .attr("width", radius).attr("height", radius)
-                .attr("fill", "#fff");
-
-            if (node.avatarUrl && node.avatarUrl.trim().length > 0) {
-                pattern.append("image")
-                    .attr("x", "0%").attr("y", "0%")
-                    .attr("width", radius).attr("height", radius)
-                    .attr("xlink:href", node.avatarUrl);
-            } else {
-                let text = node.name[0];
-                if (text == '!' || text == '@' || text == '#')
-                    text = node.name[1];
-                if (!text || node.isAnonymous) {
-                    text = node.type == 'room' ? "#" : "@";
-                }
-
-                let size = node.type == 'room' ? 8 : 6;
-
-                pattern.append("rect")
-                    .attr("width", radius).attr("height", radius)
-                    .attr("fill", this.getBackgroundForString(node.name));
-                pattern.append("text")
-                    .attr("text-anchor", "middle")
-                    .attr("dominant-baseline", "central")
-                    .attr("alignment-baseline", "central")
-                    .attr("x", radius / 2).attr("y", radius / 2)
-                    .attr("font-family", "sans-serif")
-                    .attr("font-size", size)
-                    .attr("fill", "#fff")
-                    .text(text);
-            }
-
-            let isNew = seenNodes.length > 0 && (seenNodes.indexOf(node.id) === -1);
-            if (!isNew || node.type == 'user') continue;
-
-            pattern.append("rect")
-                .attr("width", radius * 2).attr("height", 12)
-                .attr("fill", "#f9d35e")
-                .attr("stroke", "#a0a0a0")
-                .attr("stroke-width", "1px")
-                .attr("transform", "translate(0) rotate(45 " + radius + " 6)");
-            pattern.append("text")
-                .attr("text-anchor", "middle")
-                .attr("dominant-baseline", "central")
-                .attr("alignment-baseline", "central")
-                .attr("x", 11).attr("y", 9)
-                .attr("transform", "rotate(45 " + radius + " 6)")
-                .attr("font-family", "sans-serif")
-                .attr("font-size", 4)
-                .attr("fill", "#000")
-                .text("NEW");
-        }
-    }
-
-    private getBackgroundForString(str) {
-        let hash = str.hashCode();
-        if (hash < 0) hash = hash * -1;
-
-        const options = [
-            "#ae71c6",
-            "#71c6a8",
-            "#a9c671",
-            "#7189c6",
-            "#c46fa8"
-        ];
-
-        return options[hash % options.length];
-    }
-
-    private getColorForType(pointOrType) {
-        switch (pointOrType.type || pointOrType) {
-            case "invite":
-                return "#10b748";
-            case "self_link":
-                return "#694bcc";
-            case "kick":
-                return "#dd5e1a";
-            case "ban":
-                return "#ff2626";
-            case "message":
-            default:
-                return "#999";
-        }
-    }
+    // private getBackgroundForString(str) {
+    //     let hash = str.hashCode();
+    //     if (hash < 0) hash = hash * -1;
+    //
+    //     const options = [
+    //         "#ae71c6",
+    //         "#71c6a8",
+    //         "#a9c671",
+    //         "#7189c6",
+    //         "#c46fa8"
+    //     ];
+    //
+    //     return options[hash % options.length];
+    // }
+    //
+    // private getColorForType(pointOrType) {
+    //     switch (pointOrType.type || pointOrType) {
+    //         case "invite":
+    //             return "#10b748";
+    //         case "self_link":
+    //             return "#694bcc";
+    //         case "kick":
+    //             return "#dd5e1a";
+    //         case "ban":
+    //             return "#ff2626";
+    //         case "message":
+    //         default:
+    //             return "#999";
+    //     }
+    // }
 
     private processNetwork(network: VoyagerNetwork) {
         const nodes = [];
