8fc7e0796a89bd4b6e1f28e5671a32e8220523e5
turt2live <travpc@gmail.com>
2017-09-03 14:52:42 -0600

    Start of a template to use Vue

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    parser: 'postcss-scss',
-    plugins: {
-        'postcss-import': {},
-        'postcss-cssnext': {browsers: ['last 2 version'], warnForDuplicates: false},
-        'cssnano': {}
-    }
-};
