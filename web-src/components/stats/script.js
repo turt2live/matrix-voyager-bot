cb5b558be42db6fcc383933a0532e814430946e3
turt2live <travpc@gmail.com>
2017-09-08 20:13:35 -0600

    Add column sorting to stats page.
    Adds #193

--- web-src/components/stats/script.js
+++ web-src/components/stats/script.js
@@ -1,8 +1,9 @@
 import StatBox from "./stat-box/stat-box.vue";
+import SortIcon from "./sort-icon/sort-icon.vue";
 
 export default {
     name: 'stats',
-    components: {StatBox},
+    components: {StatBox, SortIcon},
     data () {
         return {
             isLoading: true,
@@ -14,9 +15,32 @@ export default {
                 mentions: 10000,
                 servers: 24576
             },
+            sortBy: "id",
+            sortDir: "natural",
             rooms: [],
             seenRooms: JSON.parse(localStorage.getItem("t2l-voyager.seenNodes") || "[]")
         };
+    },
+    computed: {
+        sortedRooms: function() {
+            var getProp = (item, prop) => {
+                if (prop === "name") return item.meta.displayName;
+                if (prop === "alias") return item.meta.primaryAlias;
+                if (prop === "users") return item.meta.stats.users;
+                if (prop === "servers") return item.meta.stats.servers;
+                if (prop === "aliases") return item.meta.stats.aliases;
+                if (prop === "id") return item.id;
+                return item[prop];
+            };
+
+            return this.rooms.sort((a, b) => {
+                var order = 0;
+                if (getProp(a, this.sortBy) < getProp(b, this.sortBy)) order = -1;
+                if (getProp(a, this.sortBy) > getProp(b, this.sortBy)) order = 1;
+
+                return this.sortDir === "asc" ? order : -order;
+            });
+        }
     },
     mounted () {
         this.$http.get('/api/v1/stats').then(response => {
@@ -41,6 +65,19 @@ export default {
         isNew (item) {
             if (this.seenRooms.length === 0) return false;
             return this.seenRooms.indexOf(item.id) === -1;
+        },
+        setSort (column) {
+            if (this.sortBy !== column) {
+                this.sortBy = column;
+                this.sortDir = "asc";
+            } else {
+                if (this.sortDir === "asc") this.sortDir = "desc";
+                else if (this.sortDir === "desc") this.sortDir = "natural";
+                else if (this.sortDir === "natural") this.sortDir = "asc";
+            }
+
+            if (this.sortDir === "natural")
+                this.sortBy = 'id';
         }
     }
 };
---
+++ web-src/components/stats/sort-icon/script.js
@@ -0,0 +1,4 @@
+export default {
+    name: 'sort-icon',
+    props: ['current', 'direction', 'watch']
+};
