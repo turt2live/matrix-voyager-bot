7d29f858ad9f3144a515e3c9dc200e56d2da4b3a
Travis Ralston <travpc@gmail.com>
2018-12-23 15:07:56 -0700

    Early handling of appservice events

--- src/appservice/worker.ts
+++ src/appservice/worker.ts
@@ -4,6 +4,7 @@ import { VoyagerConfig } from "../VoyagerConfig";
 import * as escapeStringRegexp from "escape-string-regexp";
 import * as mkdirp from "mkdirp";
 import * as path from "path";
+import { LogService } from "matrix-js-snippets";
 
 /**
  * Creates an appservice worker
@@ -38,10 +39,54 @@ export class AppserviceWorker implements IWorker {
             joinStrategy: new SimpleRetryJoinStrategy(),
             registration: registration,
         });
+
+        // We don't listen to any of the other appservice events because we have a .* user
+        // namespace, which means events fire for *everything*.
+        this.appservice.on("room.event", this.handleEvent.bind(this));
     }
 
     start(): Promise<any> {
         return this.appservice.begin();
+    }
+
+    private handleEvent(roomId: string, event: any) {
+        if (event["type"] === "m.room.member" && event["state_key"] === this.appservice.botUserId) {
+            this.processMembershipEvent(roomId, event);
+        }
+        if (event["type"] === "m.room.message" && event["content"]) {
+            this.processMessageEvent(roomId, event);
+        }
+        if (event["state_key"] || event["state_key"] === '') {
+            this.processStateEvent(roomId, event);
+        }
+    }
+
+    private processMembershipEvent(roomId: string, event: any) {
+        if (!event["content"]) return;
+
+        const membership = event["content"]["membership"];
+        if (membership === "invite") {
+            LogService.info("AppserviceWorker", "RECEIVED INVITE");
+            this.appservice.botIntent.joinRoom(roomId);
+        } else if (membership === "join") {
+            LogService.info("AppserviceWorker", "JOINED ROOM");
+        } else if (membership === "leave") {
+            if (event["sender"] !== event["state_key"]) {
+                LogService.info("AppserviceWorker", "KICKED FROM ROOM");
+            } else {
+                LogService.info("AppserviceWorker", "LEFT ROOM PEACEFULLY");
+            }
+        } else if (membership === "ban") {
+            LogService.info("AppserviceWorker", "BANNED FROM ROOM");
+        }
+    }
+
+    private processMessageEvent(roomId: string, event: any) {
+        LogService.info("AppserviceWorker", "TODO: Process message");
+    }
+
+    private processStateEvent(roomId: string, event: any) {
+        LogService.info("AppserviceWorker", "TODO: Process state event");
     }
 
     /**
@@ -49,10 +94,10 @@ export class AppserviceWorker implements IWorker {
      * @returns {IAppserviceRegistration} the registration
      */
     static generateRegistrationFromConfig(): IAppserviceRegistration {
-        return <any>{
+        return {
             id: "voyager",
-            hsToken: VoyagerConfig.appservice.hsToken,
-            asToken: VoyagerConfig.appservice.asToken,
+            hs_token: VoyagerConfig.appservice.hsToken,
+            as_token: VoyagerConfig.appservice.asToken,
             url: `http://localhost:${VoyagerConfig.web.port}`,
             sender_localpart: VoyagerConfig.matrix.userLocalpart,
             namespaces: {
