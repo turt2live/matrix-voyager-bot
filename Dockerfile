4f56af28d8b286217aec53866e567ed83fb05ddf
Travis Ralston <travpc@gmail.com>
2021-10-01 13:11:00 -0600

    Fix docker

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,8 @@ COPY . /app
 RUN chown -R node /app
 USER node
 RUN npm install
+RUN npm run build
 ENV NODE_ENV=production
 VOLUME ["/app/db", "/app/config"]
-
+EXPOSE 8184
 CMD node index.js
--- docker.sh
+++
@@ -1,4 +0,0 @@
-docker build -t t2bot/matrix-voyager-bot:web-latest -f Dockerfile.web .
-docker build -t t2bot/matrix-voyager-bot:bot-latest -f Dockerfile.bot .
-
-echo '## docker push t2bot/matrix-voyager-bot:web-latest && docker push t2bot/matrix-voyager-bot:bot-latest'
